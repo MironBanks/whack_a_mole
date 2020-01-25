d47d0c99a1c3d4bed4419c9da57b7b67dfb67853
Miron Banks <madapegarage@gmail.com>
2020-01-25 04:18:51 +0100

    finish

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+// create var
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+// Function to pick random holes
+function randomHole(holes) {
+	const idx = Math.floor(Math.random() * holes.length);
+	const hole = holes[idx];
+	// if will be same number then return function again
+	if (hole === lastHole) {
+		console.log('Ah nah thats the same one bud');
+		return randomHole(holes);
+	}
+
+	lastHole = hole;
+	return hole;
+}
+
+// Function to get Moles UP!
+function peep() {
+	const time = randomTime(200, 1000);
+	const hole = randomHole(holes);
+	hole.classList.add('up');
+	setTimeout(() => {
+		hole.classList.remove('up');
+		if (!timeUp) peep();
+	}, time);
+}
+
+//Function Start Game
+function startGame() {
+	scoreBoard.textContent = 0;
+	timeUp = false;
+	score = 0;
+	peep();
+	setTimeout(() => (timeUp = true), 10000);
+}
+
+// Function click on MOLS
+function bonk(e) {
+	if (!e.isTrusted) return;
+	score++;
+	this.classList.remove('up');
+	scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
